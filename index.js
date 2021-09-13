f3d380032b733c015e1d3c11e3ff36a2c3012ce5
Natalia Baldivia Gomes <69177421+natbaldivia@users.noreply.github.com>
2021-09-12 21:37:23 -0300

    criação de Funcionários do banco e autenticação

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import {Cliente} from "./Cliente.js"
-import { Conta } from "./Conta.js";
-import {ContaCorrente} from "./ContaCorrente.js"
-import {ContaPoupanca} from "./ContaPoupanca.js"
-import {ContaSalario} from "./ContaSalario.js"
+import {Gerente } from "./Funcionarios/Gerente.js"
+import {Diretor } from "./Funcionarios/Diretor.js"
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456789");
+const gerente = new Gerente("Ricardo", 5000, 12378945601);
+gerente.cadastrarSenha("123");
+const cliente = new Cliente("Sara", 34543212354, "456");
 
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456789");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
-
-console.log(contaSalario);
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
